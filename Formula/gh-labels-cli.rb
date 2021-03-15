db87014763546e22721d9c971081ba7607e30fd4
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-16 00:20:09 +0100

    chore: fix linting errors

--- Formula/gh-labels-cli.rb
+++ Formula/gh-labels-cli.rb
@@ -1,12 +1,11 @@
 class GhLabelsCli < Formula
   desc "CLI for managing labels in existing GitHub repositories"
   homepage "https://github.com/SirWindfield/gh-labels-cli"
-  url "https://github.com/SirWindfield/gh-labels-cli/archive/v0.1.3.tar.gz"
-  sha256 "7b2ad2cab79ff271c6e6a940fed836decea268453cf0b3d8b401ecddc6c0b34a"
+  url "https://github.com/SirWindfield/gh-labels-cli/archive/v0.1.5.tar.gz"
+  sha256 "ad66dedf7fcd42d9c3cac0430a6e3d667cf4f197103392aa09635e5e2f3a2f84"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/SirWindfield/gh-labels-cli.git"
 
-  depends_on "openssl@1.1"
   depends_on "rust" => :build
 
   def install
