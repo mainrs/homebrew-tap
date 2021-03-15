5e1762ea47eb107d6f2df6182f0faccb4b0282f2
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-15 14:06:41 +0100

    chore: fix linting errors

--- Formula/gh-labels-cli.rb
+++ Formula/gh-labels-cli.rb
@@ -1,10 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class GhLabelsCli < Formula
-  desc "A CLI for managing labels in existing GitHub repositories"
+  desc "CLI for managing labels in existing GitHub repositories"
   homepage "https://github.com/SirWindfield/gh-labels-cli"
-  head "https://github.com/SirWindfield/gh-labels-cli.git"
   url "https://github.com/SirWindfield/gh-labels-cli/archive/v0.1.1.tar.gz"
+  head "https://github.com/SirWindfield/gh-labels-cli.git"
   sha256 "01eb373711c856259be812a06decd9d7bf81e38b97bc60a7e658106fcbcc8a5f"
   license any_of: ["Apache-2.0", "MIT"]
 
@@ -15,15 +13,6 @@ class GhLabelsCli < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gh-labels-cli`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/gh-labels help"
   end
 end
