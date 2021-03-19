caef797e096be2e4e1c456c54c9ff87607e18591
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-19 20:20:04 +0100

    aasd

--- Formula/gh-labels-cli.rb
+++ Formula/gh-labels-cli.rb
@@ -2,7 +2,7 @@ class GhLabelsCli < Formula
   desc "CLI for managing labels in existing GitHub repositories"
   homepage "https://github.com/SirWindfield/gh-labels-cli"
   url "https://github.com/SirWindfield/gh-labels-cli/archive/v0.1.8.tar.gz"
-  sha256 "ad66dedf7fcd42d9c3cac0430a6e3d667cf4f197103392aa09635e5e2f3a2f84"
+  sha256 "856c0e24d4bcee82787a78d3504f1415acc1b9cdb54fcb6f47b2b731b005905f"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/SirWindfield/gh-labels-cli.git"
 
