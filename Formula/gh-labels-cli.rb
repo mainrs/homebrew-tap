cdfd0e125a807aa234b0eee768ea08429fd12f3a
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-19 20:02:43 +0100

    chore: update to 0.1.8

--- Formula/gh-labels-cli.rb
+++ Formula/gh-labels-cli.rb
@@ -1,7 +1,7 @@
 class GhLabelsCli < Formula
   desc "CLI for managing labels in existing GitHub repositories"
   homepage "https://github.com/SirWindfield/gh-labels-cli"
-  url "https://github.com/SirWindfield/gh-labels-cli/archive/v0.1.5.tar.gz"
+  url "https://github.com/SirWindfield/gh-labels-cli/archive/v0.1.8.tar.gz"
   sha256 "ad66dedf7fcd42d9c3cac0430a6e3d667cf4f197103392aa09635e5e2f3a2f84"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/SirWindfield/gh-labels-cli.git"
