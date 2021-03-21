ec933de52f3798d9a0a9c52c62754b16974da4ae
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-21 23:49:46 +0100

    update version

--- Formula/git-cm.rb
+++ Formula/git-cm.rb
@@ -1,7 +1,7 @@
 class GitCm < Formula
   desc "CLI for creating conventional-commits friendly commit messages"
   homepage "https://github.com/SirWindfield/git-cm"
-  url "https://github.com/SirWindfield/git-cm/archive/v0.1.4.tar.gz"
+  url "https://github.com/SirWindfield/git-cm/archive/v0.2.0.tar.gz"
   sha256 "cb77242304cb3467726fd4dea04161419a23e663bfc406736c4559c27f69306f"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/SirWindfield/git-cm.git"
