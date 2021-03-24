ba33de73270c673b66e4d8aedd8bd326681f01fa
SirWindfield <5113257+SirWindfield@users.noreply.github.com>
2021-03-24 16:44:47 +0000

    git-work: add 0.1.0 bottle.

--- Formula/git-work.rb
+++ Formula/git-work.rb
@@ -6,6 +6,12 @@ class GitWork < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/SirWindfield/git-work.git"
 
+  bottle do
+    root_url "https://github.com/SirWindfield/homebrew-tap/releases/download/git-work-0.1.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "e0a2d511c441f1278b39c2dabeb202992d7b57968e8f0169b53114432fa6c7eb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f792dbf3646d46fcae8663398ece9c2cd3103a1f5641a532e0c509b703ec9395"
+  end
+
   depends_on "rust" => :build
   uses_from_macos "zlib"
 
