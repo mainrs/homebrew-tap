ed40cc0df9ed608bb106983328109b272a03f655
SirWindfield <SirWindfield@users.noreply.github.com>
2021-05-14 19:47:43 +0200

    Update git-cm.rb
    Closes #12.
    
    Signed-off-by: SirWindfield <5113257+SirWindfield@users.noreply.github.com>

--- Formula/git-cm.rb
+++ Formula/git-cm.rb
@@ -2,7 +2,7 @@ class GitCm < Formula
   desc "CLI for creating conventional-commits friendly commit messages"
   homepage "https://github.com/SirWindfield/git-cm"
   url "https://github.com/SirWindfield/git-cm/archive/v0.2.3.tar.gz"
-  sha256 "1A8960058F7EAFF99098E80D9F91017515ED1C2D5F2C337F756A553FFC4A4FF9"
+  sha256 "1a8960058f7eaff99098e80d9f91017515ed1c2d5f2c337f756a553ffc4a4ff9"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/SirWindfield/git-cm.git"
 
